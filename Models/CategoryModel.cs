1a783c1b2765c5e63d72814ffe3a9dc55a4955a8
Raveena Rathnam <118574384+RaveenaRathnam@users.noreply.github.com>
2024-12-05 22:14:19 +0000

    Create CategoryModel.cs

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SOA_CA2_Frontend.Models
+{
+    public class  CategoryModel
+    {
+        public int category_Id { get; set; }
+        public string categoryName { get; set; }
+    }
+
+}
