af701707537c288ff64bb17905dcac53838c7411
Raveena Rathnam <118574384+RaveenaRathnam@users.noreply.github.com>
2024-12-06 15:08:02 +0000

    Create Models Related to Orders

---
+++ Models/OrderItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SOA_CA2_Frontend.Models
+{
+    public class OrderItemModel
+    {
+            public int orderItem_Id { get; set; }
+            public int order_Id { get; set; }
+            public int product_Id { get; set; }
+            public int quantity { get; set; }
+            public float price { get; set; }
+    }
+}
---
+++ Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SOA_CA2_Frontend.Models
+{
+    public class OrderModel
+    {
+            public int order_Id { get; set; }
+            public int user_Id { get; set; }
+            public DateTime order_Date { get; set; }
+            public float total_Amount { get; set; }
+            public int status { get; set; }
+            public DateTime createdAt { get; set; }
+             public List<OrderItemModel> orderItems { get; set; } = new();
+
+    }
+}
+
